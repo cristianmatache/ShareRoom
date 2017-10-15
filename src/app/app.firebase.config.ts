df72879736f8f899e3072d6b6a50a88799ac15e3
SilviaSapora <silvia.sapora@gmail.com>
2017-10-15 15:46:42 +0100

    Login + Register implemented

---
+++ src/app/app.firebase.config.ts
@@ -0,0 +1,9 @@
+export const FIREBASE_CONFIG =
+  {
+    apiKey: "AIzaSyADIVtyTlLfYSZr3G1twzm1nzRuv11zxQM",
+    authDomain: "shareroom-11e1c.firebaseapp.com",
+    databaseURL: "https://shareroom-11e1c.firebaseio.com",
+    projectId: "shareroom-11e1c",
+    storageBucket: "shareroom-11e1c.appspot.com",
+    messagingSenderId: "294991621639"
+  };
