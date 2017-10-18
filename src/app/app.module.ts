03a69437101f9a65b5f98954746d5fef42004942
SilviaSapora <silvia.sapora@gmail.com>
2017-10-18 18:34:22 +0100

    Facebook and Google auth added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,19 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-import { AngularFireModule } from 'angularfire2';
-import { AngularFireAuthModule }from 'angularfire2/auth';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home'
 import { LoginPage } from '../pages/login/login'
 import { FIREBASE_CONFIG } from "./app.firebase.config";
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule, AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { Facebook } from '@ionic-native/facebook';
+import * as firebase from 'firebase';
+import 'firebase/auth';
+import 'firebase/database';
 
 @NgModule({
   declarations: [
@@ -21,6 +27,7 @@ import { FIREBASE_CONFIG } from "./app.firebase.config";
     BrowserModule,
     IonicModule.forRoot(MyApp),
     AngularFireModule.initializeApp(FIREBASE_CONFIG),
+    AngularFireDatabaseModule,
     AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
@@ -32,6 +39,8 @@ import { FIREBASE_CONFIG } from "./app.firebase.config";
   providers: [
     StatusBar,
     SplashScreen,
+    Facebook,
+    AngularFireDatabaseModule,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
