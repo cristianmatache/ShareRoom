3b6a2eb8894698eaa14b937a44321eb8c5a3dd63
Silvia Sapora <silvia.sapora@google.com>
2017-10-19 18:49:19 +0100

    Get user info functions added

--- src/providers/database.ts
+++ src/providers/database.ts
@@ -1,6 +1,7 @@
 import { Component, Injectable } from '@angular/core';
 import { User } from '../models/user';
 import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule, AngularFireDatabase } from 'angularfire2/database';
 import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase/app';
 import { Platform } from 'ionic-angular';
@@ -9,16 +10,13 @@ import { Facebook } from '@ionic-native/facebook';
 @Injectable()
 export class Database {
 
-  constructor(private afAuth: AngularFireAuth,
+  constructor(private afAuth: AngularFireAuth, private afData: AngularFireDatabase,
     private fb: Facebook, private platform: Platform) {
   }
 
-  login(user: User) : Promise {
+  login(user: User) : Promise<any> {
     try {
       const result = this.afAuth.auth.signInWithEmailAndPassword(user.email, user.password);
-      if (result) {
-        this.navCtrl.setRoot('HomePage');
-      }
       return result;
     }
     catch (e) {
@@ -27,7 +25,7 @@ export class Database {
     }
   }
 
-  registerEmail(user: User) : Promise {
+  registerEmail(user: User) : Promise<any> {
     try {
       const result = this.afAuth.auth.createUserWithEmailAndPassword(user.email, user.password);
       console.log("User created");
@@ -39,22 +37,104 @@ export class Database {
     }
   }
 
-  facebookLogin() : Promise<User> {
+  // facebookRegister() : Promise<any> {
+  //   user : User = facebookLogin();
+  //
+  // }
+
+  facebookRegister() : Promise<any> {
     if (this.platform.is('cordova')) {
       return this.fb.login(['email', 'public_profile']).then(res => {
+        //TODO: add user picture and name here as well
         const facebookCredential = firebase.auth.FacebookAuthProvider.credential(res.authResponse.accessToken);
-        return firebase.auth().signInWithCredential(facebookCredential);
+        firebase.auth().signInWithCredential(facebookCredential);
       })
     }
     else {
       return this.afAuth.auth
         .signInWithPopup(new firebase.auth.FacebookAuthProvider())
-        .then(res => console.log(res));
+        .then(res => {
+          this.setProfilePicture(res.user.uid, res.additionalUserInfo.profile.picture.data.url);
+          this.setUserFirstName(res.user.uid, res.additionalUserInfo.profile.first_name);
+          this.setUserLastName(res.user.uid, res.additionalUserInfo.profile.last_name);
+        });
     }
   }
 
-  googleLogin() : Promise {
+  googleLogin() : Promise<any> {
     return this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+  }
+
+  setProfilePicture(userId, imageUrl) : Promise<any> {
+    return firebase.database().ref('users/' + userId).child('profile_picture').set(imageUrl);
+  }
+
+  setUserFirstName(userId, firstName) : Promise<any> {
+    return firebase.database().ref('users/' + userId).child('first_name').set(firstName);
+  }
+
+  setUserLastName(userId, lastName) : Promise<any> {
+    return firebase.database().ref('users/' + userId).child('last_name').set(lastName);
+  }
+
+  getCurrentUserId() : any {
+    if (firebase.auth().currentUser) {
+      console.log(firebase.auth().currentUser.uid);
+      return firebase.auth().currentUser.uid;
+    } else {
+      return null;
+    }
+  }
+
+  getCurrentUserProfilePicture() : any {
+    var userId = this.getCurrentUserId();
+    if (userId) {
+      firebase.database().ref('/users/' + userId + '/profile_picture').once('value',
+        function(snapshot) {
+          console.log(snapshot.val());
+        }, function (errorObject) {
+          console.log("The read failed: " + errorObject.code);
+        });
+    } else {
+      console.log("You need to log in");
+    }
+  }
+
+  getCurrentUserFirstName() : any {
+    var userId = this.getCurrentUserId();
+    if (userId) {
+      firebase.database().ref('/users/' + userId + '/first_name').once('value',
+        function(snapshot) {
+          console.log(snapshot.val());
+        }, function (errorObject) {
+          console.log("The read failed: " + errorObject.code);
+        });
+    } else {
+      console.log("You need to log in");
+    }
+  }
+
+  getCurrentUserLastName() : any {
+    var userId = this.getCurrentUserId();
+    if (userId) {
+      firebase.database().ref('/users/' + userId + '/last_name').once('value',
+        function(snapshot) {
+          console.log(snapshot.val());
+        }, function (errorObject) {
+          console.log("The read failed: " + errorObject.code);
+        });
+    } else {
+      console.log("You need to log in");
+    }
+  }
+
+  addFriend(userId_1, userId_2) {
+    firebase.database().ref('/users/' + userId_1 + '/friends').push().set({
+      userId_2
+    });
+    firebase.database().ref('/users/' + userId_2 + '/friends').push().set({
+      userId_1
+    });
   }
 
 }
