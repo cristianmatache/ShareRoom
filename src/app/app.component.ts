64a9a29ad47a9d8e7e48f3451b64de9778033e4b
SilviaSapora <silvia.sapora@gmail.com>
2017-10-20 16:29:29 +0100

    Tests added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,13 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { HomePage } from '../pages/home/home';
-import { LoginPage } from '../pages/login/login';
 import {ChatPage} from "../pages/chat/chat";
 @Component({
   templateUrl: 'app.html'
 })
-export class MyApp {
+export class ShareRoom {
   rootPage:any = ChatPage;
+  pages:number =1;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
-import { MyApp } from './app.component';
+import { ShareRoom } from './app.component';
 import { HomePage } from '../pages/home/home'
 import { LoginPage } from '../pages/login/login'
 import { FIREBASE_CONFIG } from "./app.firebase.config";
@@ -20,21 +20,21 @@ import { Chat } from "../providers/chat";
 
 @NgModule({
   declarations: [
-    MyApp,
+    ShareRoom,
     HomePage,
     LoginPage,
     ChatPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp),
+    IonicModule.forRoot(ShareRoom),
     AngularFireModule.initializeApp(FIREBASE_CONFIG),
     AngularFireDatabaseModule,
     AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
+    ShareRoom,
     HomePage,
     LoginPage,
     ChatPage
