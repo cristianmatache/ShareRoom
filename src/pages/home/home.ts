b2124b5e9d6a5fae6b2f25db5bf8e8c0a76a5de1
Alexandru Toma <alexandru.toma15@imperial.ac.uk>
2017-10-29 15:00:08 +0000

    Made grid codeable

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { MenuController } from 'ionic-angular';
 import { MapPage } from '../map/map';
+import {Item} from "../../models/item";
 
 @Component({
   selector: 'page-home',
@@ -11,12 +12,68 @@ import { MapPage } from '../map/map';
 
 export class HomePage {
 
-  constructor(public navCtrl: NavController, public menuCtrl:MenuController) {
+  items: Item[] = [];
 
+  constructor(public navCtrl: NavController, public menuCtrl: MenuController) {
+    this.items.push({
+      name: "Bed",
+      location: [-1, 1],
+      owner_uid: "owner",
+      picture: "../../assets/image/bed.jpg",
+      description: "This is a bed",
+      date_posted: new Date().getTime()
+    }, {
+      name: "Bed",
+      location: [-1, 1],
+      owner_uid: "owner",
+      picture: "../../assets/image/bicycle.JPG",
+      description: "This is a bed",
+      date_posted: new Date().getTime()
+    }, {
+      name: "Bed",
+      location: [-1, 1],
+      owner_uid: "owner",
+      picture: "../../assets/image/marty-avatar.png",
+      description: "This is a bed",
+      date_posted: new Date().getTime()
+    }, {
+      name: "Bed",
+      location: [-1, 1],
+      owner_uid: "owner",
+      picture: "../../assets/image/tv.jpg",
+      description: "This is a bed",
+      date_posted: new Date().getTime()
+    }, {
+      name: "Bed",
+      location: [-1, 1],
+      owner_uid: "owner",
+      picture: "../../assets/image/bed.jpg",
+      description: "This is a bed",
+      date_posted: new Date().getTime()
+    });
+  }
+
+  ionViewDidLoad() {
+    this.refreshItems();
+  }
+
+  refreshItems() {
   }
 
   changeToMap() {
     this.navCtrl.setRoot(MapPage);
+  }
+
+  getDistanceTill(item) {
+    return "15miles";
+  }
+
+  getNumberOfColumns() {
+    var nrList = [];
+    for (var i = 0; i < Math.floor(window.innerWidth / 360); i++) {
+      nrList.push(i);
+    }
+    return nrList;
   }
 
 }
