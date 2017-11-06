65b214d9c5347055ea50f1b79eb5867e7b069b7e
Silvia Sapora <silvia.sapora@gmail.com>
2017-11-06 01:18:38 +0000

    Item page for items posted by the current user implemented with ability to edit and remove the item

---
+++ src/pages/item-by-user/item-by-user.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Item} from "../../models/item";
+import {User} from "../../models/user";
+import {Database} from "../../providers/database";
+import {ChatPage} from "../chat/chat";
+import {EditItemPage} from "../edit-item/edit-item";
+
+/**
+ * Generated class for the ItemPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-item-by-user',
+  templateUrl: 'item-by-user.html',
+})
+export class ItemByUserPage {
+
+  item: Item = {} as Item;
+  user: User = {} as User;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private database: Database) {
+    this.item = navParams.get("item");
+
+    this.database.getUserInfoById(this.item.owner_uid)
+      .then((user) => {
+        this.user = user;
+      })
+      .catch(console.error);
+  }
+
+  getPicture() {
+    return this.item.picture ? this.item.picture : "";
+  }
+
+  removeItem() {
+    this.database.removeItem(this.item.id, this.database.getCurrentUserId());
+  }
+
+  editItem() {
+    this.navCtrl.push(EditItemPage, {item: this.item});
+  }
+}
